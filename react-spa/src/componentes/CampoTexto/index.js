41c2973f7fb589f03be90ca1d750a1ff46d4290a
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-11-14 16:34:31 -0300

    trabalhando com controle de estado

--- react-spa/src/componentes/CampoTexto/index.js
+++ react-spa/src/componentes/CampoTexto/index.js
@@ -2,10 +2,15 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
+
+    const aodigitado = (evento) => {
+       props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className='campo-texto'>
             <label>{props.label}</label> 
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aodigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
