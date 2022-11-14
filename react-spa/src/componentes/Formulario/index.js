56ec506f03fc012ca6a8cd3c8f4c206ac744dff4
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-11-14 15:48:38 -0300

    validando formulario com props

--- react-spa/src/componentes/Formulario/index.js
+++ react-spa/src/componentes/Formulario/index.js
@@ -1,14 +1,33 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'Ux e Design',
+        'Mobile'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('form submetido')
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados solicitados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa required={true} label="Time" itens={times}/>
+                <Botao>Criar card</Botao>
             </form>
         </section>
     )
